2bd507aaf64a107aa9031fee7754ef95aff9b7a3
Juan Castillo <cfuendesign@tutanota.com>
2022-09-22 22:33:33 -0500

    vite + lit + solidifying style

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,25 @@
+const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+const EleventyLitPlugin = require("@lit-labs/eleventy-plugin-lit");
+
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    viteOptions: {
+      clearScreen: false,
+      server: {
+        mode: "development",
+        middlewareMode: true,
+      },
+      build: {
+        mode: "production",
+      }
+    }
+  });
+  eleventyConfig.addPlugin(EleventyLitPlugin);
+
   eleventyConfig.addPassthroughCopy('./src/style');
+  eleventyConfig.addPassthroughCopy('./src/scripts');
   eleventyConfig.addWatchTarget('./src/style');
+  eleventyConfig.addWatchTarget('./src/scripts');
   eleventyConfig.addFilter('log', (value) => {
     console.log(value);
   });
