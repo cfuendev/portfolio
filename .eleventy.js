991e52367da7a3c729e502e15474867ca10be37a
Juan Castillo <cfuendesign@tutanota.com>
2022-09-28 15:26:09 -0500

    -vite +flamethrower +transitions

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,4 @@
-const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
-const EleventyLitPlugin = require("@lit-labs/eleventy-plugin-lit");
-
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addPlugin(EleventyVitePlugin, {
-    viteOptions: {
-      clearScreen: false,
-      server: {
-        mode: "development",
-        middlewareMode: true,
-      },
-      build: {
-        mode: "production",
-      }
-    }
-  });
-  eleventyConfig.addPlugin(EleventyLitPlugin);
-
   eleventyConfig.addPassthroughCopy('./src/style');
   eleventyConfig.addPassthroughCopy('./src/scripts');
   eleventyConfig.addWatchTarget('./src/style');
---
+++ public/scripts/router.js
@@ -0,0 +1,3 @@
+import flamethrower from "flamethrower-router";
+const router = flamethrower();
+document.querySelector("body").classList.add("vitae");
