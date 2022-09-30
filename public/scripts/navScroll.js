78ec5cf82b71ecbe3cb98a35ae3cc30eb7a1d06f
Juan Castillo <cfuendesign@tutanota.com>
2022-09-30 12:55:48 -0500

    fix navScroll

--- public/scripts/navScroll.js
+++ public/scripts/navScroll.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll',() => {
-	if (window.scrollY > 65) {
+	if (window.scrollY > 290) {
 		document.querySelector("header#Nav").classList.add("Scrolled")
 	} else {
 		document.querySelector("header#Nav").classList.remove("Scrolled")
