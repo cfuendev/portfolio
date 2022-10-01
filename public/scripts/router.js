782b3f24623f661f9de7eba4403cff742a00353e
Juan Castillo <cfuendesign@tutanota.com>
2022-10-01 13:21:21 -0500

    router corrections?

--- public/scripts/router.js
+++ public/scripts/router.js
@@ -12,13 +12,15 @@ const handleLocation = async () => {
     const html = parser.parseFromString(await fetch(window.location.pathname).then((data) => data.text()), "text/html");
     const head = html.querySelector("head").innerHTML;
     const body = html.querySelectorAll("body > *:not(header)");
-    document.querySelector("head").innerHTML = head;
     document.querySelectorAll("body > *:not(header)").forEach((element)=>{
         element.remove();
     })
+    document.querySelector("head").innerHTML = head;
     body.forEach((newBodyElement) => {
         document.querySelector("body").appendChild(newBodyElement);
     })
+    //document.querySelector("body > *:not(header)").classList.add("hide");
+    //document.querySelector("body > *:not(header)").classList.remove("hide");
 };
 
 window.onpopstate = handleLocation;
